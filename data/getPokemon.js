60dee618447bc52827daa811605424d63a64b8f4
unknown <huyhuynh1010@gmail.com>
2023-05-11 11:06:03 +0700

    add router and fix json file

--- data/getPokemon.js
+++ data/getPokemon.js
@@ -33,11 +33,20 @@ const editPokemonTypes = () => {
   for (let i = 0; i < data.pokemons.length; i++) {
     const pokemon = data.pokemons[i];
     let type1 = data.pokemons[i].type1;
-    let type2 = data.pokemons[i].type2 ? data.pokemons[i].type2 : null;
-    const types = [type1, type2];
+    let type2 = data.pokemons[i].type2;
+    const types =
+      type1 && type2
+        ? [type1, type2]
+        : type1 || type2
+        ? [type1 || type2]
+        : null;
+
     pokemon["types"] = types;
-    console.log(pokemon);
+    delete pokemon.type1;
+    delete pokemon.type2;
+    data.pokemons[i] = pokemon;
   }
+  fs.writeFileSync("pokemon.json", JSON.stringify(data));
 };
 
 // editPokemonTypes();
@@ -62,21 +71,34 @@ const pokemonLowerCase = () => {
 };
 // pokemonLowerCase();
 
-// const addPokemonstats = async () => {
-//   const newData = await csv().fromFile("pokemonextrafeatures.csv");
-//   console.log(newData.length);
-//   for (let i = 0; i < newData.length; i++) {
-//     const pokemon = newData[i];
-//     const stats = {
-//       abilities: pokemon.abilities,
-//       height: pokemon.height_m,
-//       weight: pokemon.weight_kg,
-//       classification: pokemon.classification,
-//     };
-//   }
-// };
+const addPokemonstats = async () => {
+  const newData = await csv().fromFile("pokemonextrafeatures.csv");
 
-// addPokemonstats();
+  let data = JSON.parse(fs.readFileSync("pokemon.json"));
+
+  for (let i = 0; i < newData.length; i++) {
+    const pokemonIndex = newData[i];
+    const abilities = pokemonIndex.abilities;
+    const height = pokemonIndex.height_m;
+    const weight = `${pokemonIndex.weight_kg} kg`;
+
+    return abilities, height, weight;
+  }
+
+  console.log(abilities);
+
+  // for (let i = 0; i < data.pokemons.length; i++) {
+  //   const pokemon = data.pokemons[i];
+  //   pokemon["abilities"] = abilities;
+  //   pokemon["height"] = height;
+  //   pokemon["weight"] = weight;
+  //   data.pokemons[i] = pokemon;
+  // }
+
+  // fs.writeFileSync("pokemon.json", JSON.stringify(data));
+};
+
+addPokemonstats();
 
 const filterPokemon = () => {
   let data = JSON.parse(fs.readFileSync("pokemon.json"));
--- routes/pokemon.api.js
+++ routes/pokemon.api.js
@@ -29,8 +29,9 @@ router.get("/", (req, res, next) => {
     }
 
     result = result.slice(offset, offset + limit);
+    const data = { data: result };
 
-    res.status(200).send(result);
+    res.status(200).send(data);
   } catch (error) {
     next(error);
   }
@@ -77,20 +78,134 @@ router.get("/:pokemonId", (req, res, next) => {
 router.post("/", (req, res, next) => {
   try {
     const { name, types, id, url } = req.body;
+    console.log(types);
     if (!name || !types || !id || !url) {
       const exception = new Error(`Missing required info`);
       exception.statusCode = 401;
       throw exception;
     }
+    if (types.length > 2) {
+      const exception = new Error("Pokémon can only have one or two types");
+      exception.statusCode = 401;
+      throw exception;
+    }
+    const pokemonTypes = [
+      "bug",
+      "dragon",
+      "fairy",
+      "fire",
+      "ghost",
+      "ground",
+      "normal",
+      "psychic",
+      "steel",
+      "dark",
+      "electric",
+      "fighting",
+      "flying",
+      "grass",
+      "ice",
+      "poison",
+      "rock",
+      "water",
+    ];
+
+    types.forEach((type) => {
+      if (!pokemonTypes.includes(type)) {
+        const exception = new Error("Pokémon's type is invalid");
+        exception.statusCode = 401;
+        throw exception;
+      }
+    });
 
     let db = JSON.parse(fs.readFileSync("pokemon.json", "utf-8"));
     const { pokemons } = db;
-    const type1 = pokemons.forl;
-    const type2 = pokemons.type2;
-    console.log(type1);
-    const newPokemon = { Name, Type1, Type2, id, image };
+
+    const newPokemon = { name, types, id, url };
+
+    pokemons.forEach((pokemon) => {
+      const pokemonName = pokemon.name;
+      const pokemonId = pokemon.id;
+      if (pokemonName === name || pokemonId === id) {
+        const exception = new Error(
+          "Name or Id already exists in the database"
+        );
+        exception.statusCode = 401;
+        throw exception;
+      }
+    });
+
+    pokemons.push(newPokemon);
+    db.pokemons = pokemons;
+    fs.writeFileSync("pokemon.json", JSON.stringify(db));
+
+    res.status(200).send(newPokemon);
   } catch (error) {
     next(error);
   }
 });
 module.exports = router;
+
+router.put("/:pokemonId", (req, res, next) => {
+  try {
+    const allowedUpdate = ["name", "types", "url"];
+    const { pokemonId } = req.params;
+    const update = req.body;
+    const updateKeys = Object.keys(update);
+    const notAllow = updateKeys.filter((key) => !allowedUpdate.includes(key));
+
+    if (notAllow.length) {
+      const exception = new Error("Update field not allow");
+      exception.statusCode = 401;
+      throw exception;
+    }
+
+    let db = fs.readFileSync("pokemon.json", "utf-8");
+    db = JSON.parse(db);
+    const { pokemons } = db;
+    const targetIndex = pokemons.findIndex(
+      (pokemon) => pokemon.id === pokemonId
+    );
+
+    if (targetIndex < 0) {
+      const exception = new Error(`Book not found`);
+      exception.statusCode = 404;
+      throw exception;
+    }
+
+    const updatedPokemon = { ...db.pokemons[targetIndex], ...update };
+    db.pokemons[targetIndex] = updatedPokemon;
+
+    fs.writeFileSync("pokemon.json", JSON.stringify(db));
+
+    res.status(200).send(updatedPokemon);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:pokemonId", (req, res, next) => {
+  try {
+    const { pokemonId } = req.params;
+    let db = fs.readFileSync("pokemon.json", "utf-8");
+    db = JSON.parse(db);
+    const { pokemons } = db;
+
+    const targetIndex = pokemons.findIndex(
+      (pokemon) => pokemon.id === pokemonId
+    );
+    if (targetIndex < 0) {
+      const exception = new Error(`Pokemon not found`);
+      exception.statusCode = 404;
+      throw exception;
+    }
+
+    db.pokemons = pokemons.filter((pokemon) => pokemon.id !== pokemonId);
+
+    db = JSON.stringify(db);
+    fs.writeFileSync("pokemon.json", db);
+    res.status(200).send({});
+  } catch (error) {
+    next(error);
+  }
+});
